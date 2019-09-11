874aa5904f0d9fe5ade59e325cf32b7555e29220
Manish Agrawal <manish.agrawal@gmail.com>
2019-09-11 11:12:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L2_Algorithms_DataStructures
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
