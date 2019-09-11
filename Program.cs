40d8ee4d36b4295737cc62e5050ddcfc25b74be5
Manish Agrawal <manish.agrawal@gmail.com>
2019-09-11 11:22:47 -0400

    Created project

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace L2_Algorithms_DataStructures
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      // Array examples
+      // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/single-dimensional-arrays
+
+      int[] ExampleArray = new int[5];
+      ExampleArray[0] = 0;
+      ExampleArray[1] = 1;
+
+      Console.WriteLine(ExampleArray[0]);
+      Console.WriteLine(ExampleArray[1]);
+      Console.WriteLine(ExampleArray[2]); // what to expect here?
+
+      //Alternate forms of initialization
+      string[] weekDays1 = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+      string[] weekDays2 = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+      // Exercise: print the element with content "Thu"
     }
   }
 }
