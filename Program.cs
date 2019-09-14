05fb3421a232404a4f71ef1e5202b502755d409e
Manish Agrawal <manish.agrawal@gmail.com>
2019-09-14 09:01:02 -0400

    Added initial set of exercises

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace L2_Algorithms_DataStructures
 {
@@ -22,6 +23,50 @@ namespace L2_Algorithms_DataStructures
       string[] weekDays2 = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 
       // Exercise: print the element with content "Thu"
+      Console.WriteLine(weekDays2[4]);
+
+      // multi-diemnsional arrays
+      // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/multidimensional-arrays
+
+      int[,] Array2D = new int[4, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+
+      // What will this print?
+      Console.WriteLine(Array2D[3, 1]);
+
+      // Exercise: Create a 2D array of strings - 4 rows and 3 columns
+      // each row to contain related colors (e.g. "red", "orange", "brown")
+
+      // Exercise: Print each row
+
+
+      // Hash tables/ Dictionaries
+      // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.dictionary-2?view=netframework-4.8
+      // May need to add Using System.Collections.Generic
+
+      Dictionary<string, string> openWith = new Dictionary<string, string>();
+
+      // Add some key value pairs to the dictionary
+      openWith.Add("txt", "notepad.exe");
+      openWith.Add("bmp", "paint.exe");
+      openWith.Add("dib", "paint.exe");
+      openWith.Add("rtf", "wordpad.exe");
+
+      // Why do get an error below? 
+      //openWith.Add("rtf", "word.exe");
+      
+      // How can we fix the error?
+
+
+
+      // The key is used to index values 
+      Console.WriteLine("For key = \"rtf\", value = {0}.", openWith["rtf"]);
+
+      // The values associated with a key can be changed
+      openWith["rtf"] = "winword.exe";
+      Console.WriteLine("For key = \"rtf\", value = {0}.", openWith["rtf"]);
+
+      // Delete item at key
+      openWith.Remove("doc");
     }
   }
 }
